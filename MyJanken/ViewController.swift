0a391415e05238b2ea03d5d8c98da18f68217221
alcueid <irailgun30@gmail.com>
2020-08-15 22:44:48 +0900

    new

---
+++ MyJanken/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  MyJanken
+//
+//  Created by alcueid on 2020/08/15.
+//  Copyright © 2020 alcueid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBOutlet weak var answerImageView: UIImageView!
+    
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    // じゃんけん結果の値
+    var answerNumber = 0;
+    
+    @IBAction func shuffleAction(_ sender: Any) {
+        if answerNumber == 0 {
+            answerLabel.text = "グー"
+            answerImageView.image = UIImage(named:"gu")
+        } else if answerNumber == 1 {
+            answerLabel.text = "チョキ"
+            answerImageView.image = UIImage(named:"choki")
+        } else if answerNumber == 2 {
+            answerLabel.text = "パー"
+            answerImageView.image = UIImage(named:"pa")
+        } else {
+            answerNumber = 0;
+            answerLabel.text = "グー"
+            answerImageView.image = UIImage(named:"gu")
+        }
+        answerNumber += 1
+    }
+}
+
